f7c479bfd3556e2cdb6d7710b9074f0c22fcef25
mosiwon <61589097+mosiwon@users.noreply.github.com>
2022-07-17 22:25:03 +0900

    세호파일입니다
    모델불러오기+imu, diffdrive추가

---
+++ jumpseho/launch/jplaunch.py
@@ -0,0 +1,71 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import ExecuteProcess
+from launch_ros.actions import Node
+from scripts import GazeboRosPaths
+import os
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import ThisLaunchFileDir,LaunchConfiguration
+from launch_ros.actions import Node
+from launch.actions import ExecuteProcess
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+    model_path = GazeboRosPaths.get_paths()
+
+    pkg_dir = ('/home/seho/ros2/test_ws/src/jumpseho')
+    env = {
+        "GAZEBO_MODEL_PATH": model_path,
+    }
+
+    sdf_prefix = ('/home/seho/ros2/test_ws/src/jumpseho')
+    sdf_file = os.path.join(sdf_prefix, "urdf", "jumpbot.sdf")
+
+    world_prefix = ('/home/seho/ros2/test_ws/src/jumpseho')
+    world_file = os.path.join(world_prefix, "gazebo_world", "seho.world")
+
+
+    return LaunchDescription(
+        [
+            ExecuteProcess(
+                cmd=[
+                    "gazebo",
+                    "-s",
+                    "libgazebo_ros_init.so",
+                    "-s",
+                    "libgazebo_ros_factory.so",
+                     world_file,
+                ],
+                output="screen",
+                additional_env=env,
+            ),
+            Node(
+                package="gazebo_ros",
+                node_executable="spawn_entity.py",
+                arguments=[
+                    "-entity",
+                    "jumpbot",
+                    "-x",
+                    "-1",
+                    "-y",
+                    "0",
+                    "-z",
+                    ".41",
+                    "-b",
+                    "-file",
+                    sdf_file,
+                ],
+            ),
+            Node(
+                package="robot_state_publisher",
+                node_executable="robot_state_publisher",
+                output="screen",
+                arguments=[sdf_file],
+            ),
+          
+        ]
+    )
